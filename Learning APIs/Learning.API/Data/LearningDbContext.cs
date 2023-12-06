13b077e73afda203f903fd04607d4f755dcf627e
Mantas <mantasxpuida@gmail.com>
2023-12-06 14:08:40 +0200

    Initial domain models, db context setup

---
+++ Learning APIs/Learning.API/Data/LearningDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Learning.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learning.API.Data
+{
+    public class LearningDbContext : DbContext
+    {
+        public LearningDbContext(DbContextOptions<LearningDbContext> dbContextOptions): base(dbContextOptions) { }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
--- Learning APIs/Learning.API/Program.cs
+++ Learning APIs/Learning.API/Program.cs
@@ -1,3 +1,6 @@
+using Learning.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LearningDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LearningConnectionString")));
 
 var app = builder.Build();
 
